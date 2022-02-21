fb0bcb2930033223a4ea98f53f4911ec7f23d6bd
Chlafen <bouchnak.amine03@gmail.com>
2022-02-21 04:31:02 +0100

    Added top post sliding articles + tweaked progress bar

--- client/src/pages/home/top-post/TopPost.jsx
+++ client/src/pages/home/top-post/TopPost.jsx
@@ -1,13 +1,62 @@
 import React, {useState, useEffect} from 'react';
 import './index.css';
 import LatestNews from './latest-news/LatestNews';
-import ProgressBar from './progress-bar/ProgressBar';
 import TopPostMain from './top-post-main/TopPostMain';
 
 const maxWidthToPopLatesNews = 800;
 
+const postData0 = {
+  title: 'Project Axis, a new axis newly created by IEEE INSAT',
+  category: 'Football',
+  timeOfPost: new Date(),
+  author: 'Nessrine Baltouni',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+  img: {
+    imgUrl: 'images/post1.jpg',
+    alt: 'A picture'
+  },
+  views: 158,
+  comments: 15,
+  url: '/'
+}
+
+const postData1 = {
+  title: 'Lorem ipsum axis newly created by IEEE INSAT',
+  category: 'Sports',
+  timeOfPost: new Date(),
+  author: 'Nessrine Baltouni',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+  img: {
+    imgUrl: 'images/post3.jpg',
+    alt: 'A picture'
+  },
+  views: 158,
+  comments: 15,
+  url: '/'
+}
+
+const postData2 = {
+  title: 'Pconsectetur adipiscing elit, sed do eiusmod te incidunt',
+  category: 'Science',
+  timeOfPost: new Date(),
+  author: 'Nessrine Baltouni',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+  img: {
+    imgUrl: 'images/post2.jpg',
+    alt: 'A picture'
+  },
+  views: 158,
+  comments: 15,
+  url: '/'
+}
+const cycleTime = 6000;
+
 export default function TopPost(props) {
   const [clientWidth, setClientWidth] = useState(document.documentElement.clientWidth);
+  const [current, setCurrent] = useState(0);
+  const [pgbarWidth, setPgbarWidth] = useState(100/3);
+
+  const length = 3; //nb of the posts
 
   useEffect(() => {
     function handleResize() {
@@ -17,18 +66,44 @@ export default function TopPost(props) {
     return () => window.removeEventListener('resize', handleResize);
   });
 
+  //slider + pgbar
+  useEffect(() => {
+    let evbar = document.getElementById('evolving-bar');
+    evbar.style.trasition = 'left ' + cycleTime/1000 + 's linear';
+    let interval = setInterval(() =>{
+      setCurrent(current === length-1 ? 0 : current + 1);
+      setPgbarWidth(pgbarWidth >= 60 ? 0 : pgbarWidth + 33.34);
+      evbar.style.left = pgbarWidth + '%';      
+    }, cycleTime);
+    return () => clearInterval(interval);
+  });
 
-  
-
-  const listData =[props.postData,props.postData,props.postData];
+  const listData =[postData0, postData1, postData2];
   return (
     <>
-        <div className="top-post" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}images/post4.jpg)` /* TODO: change this to be a post img url*/}}>
+      <div className="top-post ">
         <div className="mid-section">
-          <TopPostMain postData={listData[0]}/>
-          <ProgressBar/>
+        <div className="slider-imgs">
+          {listData.map((d, i) => {
+            return (
+              <div className={current === i ? "slide active" : "slide"} key={i} >
+                {i === current && (<img src={process.env.PUBLIC_URL + '/' + d.img.imgUrl} alt='img' className='slider-img'/>)}
+              </div>
+            )
+          })}
+        </div>
+          {listData.map((d, i) => {
+            return(
+              <TopPostMain postData={listData[i]} fade={current === i ? "slide-txt fade-in" : "slide-txt"} key={i}/>
+            )
+          })}
+          {/* <ProgressBar/> */}
+          <div className="progress-bar">
+            <div id="evolving-bar"></div>
+          </div>
           {clientWidth >maxWidthToPopLatesNews && <LatestNews postData={listData}/>}
         </div>
+        
       </div>
       {clientWidth <=maxWidthToPopLatesNews && <LatestNews postData={listData} style={{"margin-top":".3rem"}}/>}
     </>
--- client/src/pages/home/top-post/top-post-main/TopPostMain.jsx
+++ client/src/pages/home/top-post/top-post-main/TopPostMain.jsx
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import {MdInsertComment} from 'react-icons/md';
 import {AiFillEye} from 'react-icons/ai';
 import { timeSince } from '../../../../util/utilities';
 
+const postData = {
+  title: 'Project Axis, a new axis newly created by IEEE INSAT',
+  category: 'Football',
+  timeOfPost: new Date(),
+  author: 'Nessrine Baltouni',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+  img: {
+    imgUrl: 'images/post1.jpg',
+    alt: 'A picture'
+  },
+  views: 158,
+  comments: 15,
+  url: '/'
+}
+
+const postData1 = {
+  title: 'Lorem ipsum axis newly created by IEEE INSAT',
+  category: 'Sports',
+  timeOfPost: new Date(),
+  author: 'Nessrine Baltouni',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+  img: {
+    imgUrl: 'images/post3.jpg',
+    alt: 'A picture'
+  },
+  views: 158,
+  comments: 15,
+  url: '/'
+}
+
+const postData2 = {
+  title: 'Pconsectetur adipiscing elit, sed do eiusmod te incidunt',
+  category: 'Science',
+  timeOfPost: new Date(),
+  author: 'Nessrine Baltouni',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+  img: {
+    imgUrl: 'images/post2.jpg',
+    alt: 'A picture'
+  },
+  views: 158,
+  comments: 15,
+  url: '/'
+}
+
+
+
 const TopPostMain = (props) => {
 
-
   return (
-    <div className="top-post-main">
+    <div className={'top-post-main ' + props.fade}>
       <h2>{props.postData.category || ''}</h2>
       <p>
         <a href={props.postData.url || ''}>
