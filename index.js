d5ab7edd922e29dbe7faebad71c7ab70a17372bd
Chlafen <bouchnak.amine03@gmail.com>
2022-02-21 04:57:03 +0100

    fixed heroku integration

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const path = require("path")
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const path = require("path")
 
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
